ca023190b8a34a5e6fd91a9579c949747f8ee46a
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2023-02-22 08:55:40 +0700

    seeder

--- app/Database/Seeds/Post.php
+++ app/Database/Seeds/Post.php
@@ -8,6 +8,15 @@ class Post extends Seeder
 {
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create();
+        $post  = new \App\Models\Post();
+        for ($i = 0; $i < 20; $i++) {
+            $data = [
+                'category' => $faker->numerify(),
+                'title' => $faker->name(),
+                'description' => $faker->text(),
+            ];
+            $post->insert($data);
+        }
     }
 }
