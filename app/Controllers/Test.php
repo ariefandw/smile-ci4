259f04875b399326fbbaafea7e382ff2a1f980b0
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2023-02-23 14:18:31 +0700

    calendar

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -8,11 +8,24 @@ class Test extends BaseController
     {
         // dd(auth()->loggedIn());
         db_connect()->connect();
+        $model  = new \App\Models\Post();
+        $rows   = $model->limit(3)->find();
+        $events = [];
+        foreach ($rows as $row) {
+            $events[] = [
+                'title' => $row->title,
+                'start' => $row->created_at,
+                'end' => $row->updated_at,
+                'url' => site_url('post/edit/' . $row->id),
+            ];
+        }
+
         $os   = ['windows', 'mac', 'linux'];
         $data = [
             'nama' => 'Smile',
             'alamat' => null,
             'os' => $os,
+            'json' => json_encode($events),
         ];
         return view('test/test', $data);
     }
--- app/Views/test/test.php
+++ app/Views/test/test.php
@@ -1,21 +1,25 @@
 <?= $this->extend('layout/app'); ?>
 
+<?= $this->section('js'); ?>
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js"></script>
+<script>
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const calendarEl = document.getElementById('calendar')
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            events: <?= $json; ?>,
+        })
+        calendar.render()
+    })
+
+</script>
+<?= $this->endSection(); ?>
+
 <?= $this->section('content'); ?>
 <div class="card">
     <div class="card-body">
-        <h1>
-            Selamat Datang
-            <?= $nama; ?>
-            <?= $alamat ?? 'Jogja'; ?>
-        </h1>
-        <b>Sistem Operasi:</b>
-        <ul>
-            <?php foreach ($os as $r): ?>
-                <li>
-                    <?= $r; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <div id='calendar'></div>
     </div>
 </div>
 <?= $this->endSection(); ?>
